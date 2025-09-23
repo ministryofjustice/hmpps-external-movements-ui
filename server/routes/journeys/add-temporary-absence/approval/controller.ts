971b25a633dd6087cf132738e8a16e9fa079f666
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-09-23 14:02:58 +0100

    Add approval status page to Add TAP journey

---
+++ server/routes/journeys/add-temporary-absence/approval/controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express'
+import { SchemaType } from './schema'
+import { AddTapFlowControl } from '../flow'
+
+export class AbsenceApprovalController {
+  GET = async (req: Request, res: Response) => {
+    res.render('add-temporary-absence/approval/view', {
+      backUrl: AddTapFlowControl.getBackUrl(req, 'comments'),
+      requireApproval:
+        res.locals['formResponses']?.['requireApproval'] ?? req.journeyData.addTemporaryAbsence!.requireApproval,
+    })
+  }
+
+  POST = async (req: Request<unknown, unknown, SchemaType>, res: Response) => {
+    res.redirect(AddTapFlowControl.saveDataAndGetNextPage(req, { requireApproval: req.body.requireApproval }))
+  }
+}
