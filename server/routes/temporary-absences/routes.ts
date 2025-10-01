785d092d1dba40d70453063902e8362bc6d06825
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-10-01 15:48:33 +0100

    Add Tap occurence details page

--- server/routes/temporary-absences/routes.ts
+++ server/routes/temporary-absences/routes.ts
@@ -3,12 +3,14 @@ import { BaseRouter } from '../common/routes'
 import { BrowseTapOccurrencesController } from './controller'
 import { validateOnGET } from '../../middleware/validation/validationMiddleware'
 import { schemaFactory } from './schema'
+import { TapOccurrenceDetailsController } from './details/controller'
 
 export const BrowseTapOccurrencesRoutes = ({ externalMovementsService }: Services) => {
   const { router, get } = BaseRouter()
   const controller = new BrowseTapOccurrencesController(externalMovementsService)
 
   get('/', validateOnGET(schemaFactory, 'searchTerm', 'fromDate', 'toDate', 'status', 'direction'), controller.GET)
+  get('/:id', new TapOccurrenceDetailsController(externalMovementsService).GET)
 
   return router
 }
