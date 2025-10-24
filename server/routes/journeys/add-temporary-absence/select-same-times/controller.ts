09f8e50b156c97a88eb81980fe18ce2ced5dafbb
Jack Reeve <jack.reeve@justice.gov.uk>
2025-10-24 14:13:55 +0100

    MIA-945:  Rotating pattern same times

---
+++ server/routes/journeys/add-temporary-absence/select-same-times/controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express'
+import { SchemaType } from './schemas'
+
+export class SelectSameTimesController {
+  GET = async (req: Request, res: Response) => {
+    res.render('add-temporary-absence/select-same-times/view', {
+      sameTimes:
+        res.locals['formResponses']?.['sameTimes'] ??
+        req.journeyData.addTemporaryAbsence?.rotatingPatternSubJourney?.isSameTime ??
+        req.journeyData.addTemporaryAbsence?.rotatingPattern?.isSameTime,
+      backUrl: 'enter-rotating-pattern',
+    })
+  }
+
+  POST = async (req: Request<unknown, unknown, SchemaType>, res: Response) => {
+    if (req.journeyData.addTemporaryAbsence?.rotatingPatternSubJourney) {
+      req.journeyData.addTemporaryAbsence.rotatingPatternSubJourney.isSameTime = req.body.isSameTime
+    }
+
+    res.redirect(req.body.isSameTime ? 'rotating-release-return-times' : 'rotating-non-repeating-release-return-times')
+  }
+}
