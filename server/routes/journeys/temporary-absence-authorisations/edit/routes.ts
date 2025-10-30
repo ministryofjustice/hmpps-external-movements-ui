9e1e6483c52d8b4b0a6a95e187f86fea2eee938b
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-10-30 15:21:27 +0000

    MIA-1108 Display four level absence hierarchy (#110)
    * Update TAP categorisation to 4 rows

--- server/routes/journeys/temporary-absence-authorisations/edit/routes.ts
+++ server/routes/journeys/temporary-absence-authorisations/edit/routes.ts
@@ -3,13 +3,21 @@ import { BaseRouter } from '../../../common/routes'
 import { EditTapAuthorisationController } from './controller'
 import { EditAbsenceCommentsRoutes } from './comments/routes'
 import { EditStartEndDatesRoutes } from './start-end-dates/routes'
+import { EditAbsenceTypeRoutes } from './absence-type/routes'
+import { EditAbsenceSubTypeRoutes } from './absence-subtype/routes'
+import { EditReasonCategoryRoutes } from './reason-category/routes'
+import { EditAbsenceReasonRoutes } from './reason/routes'
 
-export const EditTapAuthorisationRoutes = (_services: Services) => {
+export const EditTapAuthorisationRoutes = (services: Services) => {
   const { router, get } = BaseRouter()
   const controller = new EditTapAuthorisationController()
 
   get('/', controller.GET)
 
+  router.use('/absence-type', EditAbsenceTypeRoutes(services))
+  router.use('/absence-subtype', EditAbsenceSubTypeRoutes(services))
+  router.use('/reason-category', EditReasonCategoryRoutes(services))
+  router.use('/reason', EditAbsenceReasonRoutes(services))
   router.use('/comments', EditAbsenceCommentsRoutes())
   router.use('/start-end-dates', EditStartEndDatesRoutes())
 
