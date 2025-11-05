db941614152442bc73d9504fdfac8159b5cb1aa8
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-11-05 12:13:18 +0000

    Address PR comments

--- server/routes/journeys/add-temporary-absence/absence-type/controller.ts
+++ server/routes/journeys/add-temporary-absence/absence-type/controller.ts
@@ -3,6 +3,7 @@ import ExternalMovementsService from '../../../../services/apis/externalMovement
 import { SchemaType } from './schema'
 import { absenceCategorisationMapper, getCategoryFromJourney } from '../../../common/utils'
 import { AddTapFlowControl } from '../flow'
+import { Page } from '../../../../services/auditService'
 
 export class AbsenceTypeController {
   constructor(private readonly externalMovementsService: ExternalMovementsService) {}
@@ -10,12 +11,12 @@ export class AbsenceTypeController {
   GET = async (req: Request, res: Response) => {
     const { absenceType } = getCategoryFromJourney(req.journeyData.addTemporaryAbsence!)
 
-    const entryPath = res.locals.history?.slice(-4)[0]
+    const lastLandmark = res.locals.breadcrumbs.last()
 
     res.render('add-temporary-absence/absence-type/view', {
       backUrl:
-        entryPath?.startsWith('/temporary-absence') || entryPath?.startsWith('/search-prisoner')
-          ? entryPath
+        lastLandmark && ['temp-page-2', 'temp-page-3', Page.SEARCH_PRISONER].includes(lastLandmark.alias || '')
+          ? lastLandmark.href
           : `${res.locals.prisonerProfileUrl}/prisoner/${req.journeyData.prisonerDetails!.prisonerNumber}`,
       options: (await this.externalMovementsService.getAllAbsenceTypes({ res })).items.map(absenceCategorisationMapper),
       absenceType: res.locals.formResponses?.['absenceType'] ?? absenceType?.code,
