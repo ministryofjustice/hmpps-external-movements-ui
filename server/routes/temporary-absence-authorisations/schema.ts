5c9e6fc5e5ea0bdaa5f01766a85b5ee1be9428f7
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-11-11 17:38:18 +0000

    Remove earliest/latest date wording

--- server/routes/temporary-absence-authorisations/schema.ts
+++ server/routes/temporary-absence-authorisations/schema.ts
@@ -6,8 +6,8 @@ export const schema = createSchema({
   searchTerm: z.string().optional(),
   status: z.enum(['', 'PENDING', 'APPROVED', 'DENIED', 'WITHDRAWN']),
   clear: z.string().optional(),
-  fromDate: validateDateBase('Enter the earliest release date', 'Enter a valid earliest release date'),
-  toDate: validateDateBase('Enter the latest return date', 'Enter a valid latest return date'),
+  fromDate: validateDateBase('Enter from date', 'Enter a valid from date'),
+  toDate: validateDateBase('Enter to date', 'Enter a valid to date'),
 })
 
 type SchemaType = z.infer<typeof schema>
