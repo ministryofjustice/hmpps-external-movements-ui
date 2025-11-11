d84d0ca83ded6a081f78a20f0498d0979e445851
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-11-11 10:20:50 +0000

    fix test

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,4 +1,5 @@
 import { PermissionsService } from '@ministryofjustice/hmpps-prison-permissions-lib'
+import { OsPlacesAddressService } from '@ministryofjustice/hmpps-connect-dps-shared-items'
 import logger from '../../logger'
 import { dataAccess } from '../data'
 import AuditService from './auditService'
@@ -6,7 +7,6 @@ import ExternalMovementsService from './apis/externalMovementsService'
 import config from '../config'
 import PrisonerSearchApiService from './apis/prisonerSearchService'
 import PrisonApiService from './apis/prisonApiService'
-import CustomOsPlacesAddressService from './apis/osPlacesAddressService'
 
 export const services = () => {
   const { applicationInfo, hmppsAuditClient, osPlacesApiClient, telemetryClient, authenticationClient, cacheStore } =
@@ -18,7 +18,7 @@ export const services = () => {
     externalMovementsService: new ExternalMovementsService(authenticationClient),
     prisonerSearchService: new PrisonerSearchApiService(authenticationClient),
     prisonApiService: new PrisonApiService(authenticationClient),
-    osPlacesAddressService: new CustomOsPlacesAddressService(logger, osPlacesApiClient),
+    osPlacesAddressService: new OsPlacesAddressService(logger, osPlacesApiClient),
     prisonPermissionsService: PermissionsService.create({
       prisonerSearchConfig: config.apis.prisonerSearchApi,
       authenticationClient,
