2cef405175332eaad70e249d41166ce9de4a7659
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-11-11 17:52:09 +0000

    Remove earliest/latest date wording (#132)

--- server/routes/temporary-absences/schema.ts
+++ server/routes/temporary-absences/schema.ts
@@ -3,16 +3,14 @@ import { Request } from 'express'
 import { createSchema } from '../../middleware/validation/validationMiddleware'
 import { validateDateBase } from '../../utils/validations/validateDatePicker'
 
-export const schemaFactory = async (req: Request) => {
-  const dateType = req.query['direction'] === 'IN' ? 'return date' : 'release date'
-
+export const schemaFactory = async (_req: Request) => {
   return createSchema({
     searchTerm: z.string().optional(),
     direction: z.enum(['OUT', 'IN']),
     status: z.enum(['', 'PENDING', 'SCHEDULED', 'CANCELLED', 'COMPLETED']),
     clear: z.string().optional(),
-    fromDate: validateDateBase(`Enter the earliest ${dateType}`, `Enter a valid earliest ${dateType}`),
-    toDate: validateDateBase(`Enter the latest ${dateType}`, `Enter a valid latest ${dateType}`),
+    fromDate: validateDateBase(`Enter from date`, `Enter a valid from date`),
+    toDate: validateDateBase(`Enter to date`, `Enter a valid to date`),
   })
 }
 
