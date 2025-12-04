19f288cb9436a2edd18edd7427f5e4435b759e4f
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-12-04 11:40:58 +0000

    Update TAP Occurrence Routes to include comments
    Signed-off-by: Sameer Jahangir <sameer.jahangir@justice.gov.uk>

--- server/routes/journeys/temporary-absences/edit/routes.ts
+++ server/routes/journeys/temporary-absences/edit/routes.ts
@@ -2,12 +2,14 @@ import { Services } from '../../../../services'
 import { BaseRouter } from '../../../common/routes'
 import { EditOccurrenceStartEndDatesRoutes } from './start-end-dates/routes'
 import { EditTapOccurrenceConfirmationRoutes } from './confirmation/routes'
+import { EditAbsenceCommentsRoutes } from './comments/routes'
 
 export const EditTapOccurrenceRoutes = (services: Services) => {
   const { router } = BaseRouter()
 
   router.use('/start-end-dates', EditOccurrenceStartEndDatesRoutes(services))
   router.use('/confirmation', EditTapOccurrenceConfirmationRoutes())
+  router.use('/comments', EditAbsenceCommentsRoutes(services))
 
   return router
 }
