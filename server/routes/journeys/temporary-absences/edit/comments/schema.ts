6a4117a7827f21e1c96fd3f04b0683c3a39bcc52
Sameer Jahangir <sameer.jahangir@justice.gov.uk>
2025-12-08 16:54:32 +0000

    Remove optional and add whitespace handling
    Signed-off-by: Sameer Jahangir <sameer.jahangir@justice.gov.uk>

--- server/routes/journeys/temporary-absences/edit/comments/schema.ts
+++ server/routes/journeys/temporary-absences/edit/comments/schema.ts
@@ -4,7 +4,11 @@ import { createSchema } from '../../../../../middleware/validation/validationMid
 const CHAR_COUNT_ERROR_MSG = 'The maximum character limit is 4000'
 
 export const schema = createSchema({
-  notes: z.string().max(4000, { message: CHAR_COUNT_ERROR_MSG }).optional().default(''),
+  notes: z
+    .string()
+    .max(4000, { message: CHAR_COUNT_ERROR_MSG })
+    .default('')
+    .transform(val => (val.trim().length ? val : val.trim())),
 })
 
 export type SchemaType = z.infer<typeof schema>
