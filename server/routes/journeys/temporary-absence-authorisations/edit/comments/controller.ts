74dec5ed0d15893e15c289bf1a1095d70813261b
bill-moj <167539377+bill-moj@users.noreply.github.com>
2025-12-12 16:48:30 +0000

    Add edit TAP Authorisation comments journey (#182)

--- server/routes/journeys/temporary-absence-authorisations/edit/comments/controller.ts
+++ server/routes/journeys/temporary-absence-authorisations/edit/comments/controller.ts
@@ -1,24 +1,17 @@
 import { Request, Response } from 'express'
 import { SchemaType } from './schema'
-import { FLASH_KEY__SUCCESS_BANNER } from '../../../../../utils/constants'
-import { firstNameSpaceLastName } from '../../../../../utils/formatUtils'
 
 export class EditAbsenceCommentsController {
   GET = async (req: Request, res: Response) => {
     res.render('temporary-absence-authorisations/edit/comments/view', {
-      backUrl: '../edit',
+      backUrl: req.journeyData.updateTapAuthorisation!.backUrl,
       comments:
         res.locals.formResponses?.['comments'] ?? req.journeyData.updateTapAuthorisation!.authorisation.comments,
     })
   }
 
   POST = async (req: Request<unknown, unknown, SchemaType>, res: Response) => {
-    const journey = req.journeyData.updateTapAuthorisation!
-    // TODO: send API call to apply change
-    req.flash(
-      FLASH_KEY__SUCCESS_BANNER,
-      `You’ve updated the temporary absence comments for ${firstNameSpaceLastName(req.journeyData.prisonerDetails!)}.`,
-    )
-    res.redirect(`/temporary-absence-authorisations/${journey.authorisation.id}`)
+    req.journeyData.updateTapAuthorisation!.comments = req.body.comments
+    res.redirect('change-confirmation')
   }
 }
