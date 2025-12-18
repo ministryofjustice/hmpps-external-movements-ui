c6bdb488921ef2e08f2b74b8986820beed94bd72
bill-moj <bill.yeung@digital.justice.gov.uk>
2025-12-18 14:12:04 +0000

    Fix back url and post-journey breadcrumb of Create TAP journey

--- server/routes/journeys/add-temporary-absence/routes.ts
+++ server/routes/journeys/add-temporary-absence/routes.ts
@@ -41,7 +41,15 @@ export const AddTemporaryAbsenceRoutes = (services: Services) => {
 
   get('/start/:prisonNumber', populatePrisonerDetails(services), (req, res) => {
     if (req.middleware?.prisonerData) {
-      req.journeyData.addTemporaryAbsence = {}
+      const lastLandmark = res.locals.breadcrumbs.last()
+
+      req.journeyData.addTemporaryAbsence = {
+        backUrl:
+          lastLandmark && ['temp-page-2', 'temp-page-3', Page.SEARCH_PRISONER].includes(lastLandmark.alias || '')
+            ? lastLandmark.href
+            : `${res.locals.prisonerProfileUrl}/prisoner/${req.journeyData.prisonerDetails!.prisonerNumber}`,
+        historyQuery: String(req.query['history']),
+      }
       req.journeyData.prisonerDetails = toPrisonerDetails(req.middleware.prisonerData)
       res.redirect('../absence-type')
     } else {
