efc5b4fa2c3a01e9fd79880957a8d10514e73772
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-01-08 13:05:18 +0000

    Update check-absences page to group by weeks starting on Monday

--- server/routes/journeys/add-temporary-absence/check-absences/controller.ts
+++ server/routes/journeys/add-temporary-absence/check-absences/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { addDays, addMonths, differenceInDays, endOfMonth, format, startOfMonth } from 'date-fns'
+import { addDays, subDays, addMonths, differenceInDays, endOfMonth, format, startOfMonth } from 'date-fns'
 import { AddTapFlowControl } from '../flow'
 import { getOccurrencesToMatch } from '../utils'
 
@@ -7,17 +7,17 @@ export class CheckPatternController {
   GET = async (req: Request, res: Response) => {
     req.journeyData.addTemporaryAbsence!.isCheckPattern = true
 
-    const numberOfMonthsOrWeeks =
-      req.journeyData.addTemporaryAbsence!.patternType === 'FREEFORM'
-        ? (new Date(req.journeyData.addTemporaryAbsence!.end!).getMonth() -
-            new Date(req.journeyData.addTemporaryAbsence!.start!).getMonth() +
-            13) %
-          12
-        : Math.ceil(
-            (differenceInDays(req.journeyData.addTemporaryAbsence!.end!, req.journeyData.addTemporaryAbsence!.start!) +
-              2) /
-              7,
-          )
+    res.render('add-temporary-absence/check-absences/view', {
+      backUrl: AddTapFlowControl.getBackUrl(req, 'repeating-pattern'),
+      patternType: req.journeyData.addTemporaryAbsence!.patternType,
+      periods: this.getPeriod(req),
+    })
+  }
+
+  POST = (req: Request, res: Response) =>
+    res.redirect(req.journeyData.isCheckAnswers ? 'check-answers' : 'search-locations')
+
+  private getPeriod = (req: Request) => {
     const occurrences = getOccurrencesToMatch(req).map(({ start, end }) => {
       return {
         startDate: format(start, 'yyyy-MM-dd'),
@@ -27,24 +27,46 @@ export class CheckPatternController {
       }
     })
 
-    const periods = Array.from(new Array(numberOfMonthsOrWeeks).keys()).map(idx => {
-      let startDate: string
-      let endDate: string
+    if (req.journeyData.addTemporaryAbsence!.patternType === 'FREEFORM') {
+      const numberOfMonths =
+        (new Date(req.journeyData.addTemporaryAbsence!.end!).getMonth() -
+          new Date(req.journeyData.addTemporaryAbsence!.start!).getMonth() +
+          13) %
+        12
 
-      if (req.journeyData.addTemporaryAbsence!.patternType === 'FREEFORM') {
-        startDate =
+      return Array.from(new Array(numberOfMonths).keys()).map(idx => {
+        const startDate =
           idx === 0
             ? req.journeyData.addTemporaryAbsence!.start!
             : format(addMonths(startOfMonth(new Date(req.journeyData.addTemporaryAbsence!.start!)), idx), 'yyyy-MM-dd')
-        endDate =
-          idx === numberOfMonthsOrWeeks - 1
+        const endDate =
+          idx === numberOfMonths - 1
             ? req.journeyData.addTemporaryAbsence!.end!
             : format(endOfMonth(new Date(startDate)), 'yyyy-MM-dd')
-      } else {
-        startDate = format(addDays(req.journeyData.addTemporaryAbsence!.start!, idx * 7), 'yyyy-MM-dd')
-        endDate = format(addDays(req.journeyData.addTemporaryAbsence!.start!, idx * 7 + 6), 'yyyy-MM-dd')
-        if (endDate > req.journeyData.addTemporaryAbsence!.end!) endDate = req.journeyData.addTemporaryAbsence!.end!
-      }
+
+        const overnightNewWeekDay = format(addDays(endDate, 1), 'yyyy-MM-dd')
+        const isOvernight = (o: { startDate: string; returnDate: string }) => o.returnDate !== o.startDate
+
+        return {
+          startDate,
+          endDate,
+          absences: occurrences.filter(
+            o =>
+              o.startDate >= startDate &&
+              (o.returnDate <= endDate || (isOvernight(o) && o.returnDate === overnightNewWeekDay)),
+          ),
+        }
+      })
+    }
+
+    const diffFromMonthday = (new Date(req.journeyData.addTemporaryAbsence!.start!).getDay() + 6) % 7
+    const start = format(subDays(req.journeyData.addTemporaryAbsence!.start!, diffFromMonthday), 'yyyy-MM-dd')
+    const numberOfWeeks = Math.ceil((differenceInDays(req.journeyData.addTemporaryAbsence!.end!, start) + 2) / 7)
+
+    return Array.from(new Array(numberOfWeeks).keys()).map(idx => {
+      const startDate = format(addDays(start, idx * 7), 'yyyy-MM-dd')
+      let endDate = format(addDays(start, idx * 7 + 6), 'yyyy-MM-dd')
+      if (endDate > req.journeyData.addTemporaryAbsence!.end!) endDate = req.journeyData.addTemporaryAbsence!.end!
 
       const overnightNewWeekDay = format(addDays(endDate, 1), 'yyyy-MM-dd')
       const isOvernight = (o: { startDate: string; returnDate: string }) => o.returnDate !== o.startDate
@@ -59,14 +81,5 @@ export class CheckPatternController {
         ),
       }
     })
-
-    res.render('add-temporary-absence/check-absences/view', {
-      backUrl: AddTapFlowControl.getBackUrl(req, 'repeating-pattern'),
-      patternType: req.journeyData.addTemporaryAbsence!.patternType,
-      periods,
-    })
   }
-
-  POST = (req: Request, res: Response) =>
-    res.redirect(req.journeyData.isCheckAnswers ? 'check-answers' : 'search-locations')
 }
