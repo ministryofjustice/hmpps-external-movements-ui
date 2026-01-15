1ba81f1d28e40d1a603b4a7742937f6da6fbbbfc
bill-moj <167539377+bill-moj@users.noreply.github.com>
2026-01-15 08:58:18 +0000

    Fix repeating TAP CYA bug (#222)

--- server/routes/journeys/add-temporary-absence/flow.ts
+++ server/routes/journeys/add-temporary-absence/flow.ts
@@ -217,6 +217,38 @@ export class AddTapFlowControl {
 
     // Check answers flow
     if (req.journeyData.isCheckAnswers) {
+      if (data.absenceType) {
+        if (data.absenceType.code === journey.absenceType?.code) return 'check-answers'
+        updateCategorySubJourney(req, 'ABSENCE_TYPE', data.absenceType)
+        if (!data.absenceType.nextDomain) {
+          saveCategorySubJourney(req)
+          return 'check-answers'
+        }
+        return getUrlForNextDomain(data.absenceType.nextDomain)
+      }
+      if (data.absenceSubType) {
+        if (data.absenceSubType.code === journey.absenceSubType?.code) return 'check-answers'
+        updateCategorySubJourney(req, 'ABSENCE_SUB_TYPE', data.absenceSubType)
+        if (!data.absenceSubType.nextDomain) {
+          saveCategorySubJourney(req)
+          return 'check-answers'
+        }
+        return getUrlForNextDomain(data.absenceSubType.nextDomain)
+      }
+      if (data.reasonCategory) {
+        if (data.reasonCategory.code === journey.reasonCategory?.code) return 'check-answers'
+        updateCategorySubJourney(req, 'ABSENCE_REASON_CATEGORY', data.reasonCategory)
+        if (!data.reasonCategory.nextDomain) {
+          saveCategorySubJourney(req)
+          return 'check-answers'
+        }
+        return getUrlForNextDomain(data.reasonCategory.nextDomain)
+      }
+      if (data.reason) {
+        updateCategorySubJourney(req, 'ABSENCE_REASON', data.reason)
+        saveCategorySubJourney(req)
+        return 'check-answers'
+      }
       if (data.repeat !== undefined) {
         if (journey.repeat === data.repeat) return 'check-answers'
         journey.repeat = data.repeat
@@ -269,6 +301,36 @@ export class AddTapFlowControl {
     }
 
     // Normal flow
+    if (data.absenceType) {
+      updateCategorySubJourney(req, 'ABSENCE_TYPE', data.absenceType)
+      if (!data.absenceType.nextDomain) {
+        saveCategorySubJourney(req)
+        return 'single-or-repeating'
+      }
+      return getUrlForNextDomain(data.absenceType.nextDomain)
+    }
+    if (data.absenceSubType) {
+      updateCategorySubJourney(req, 'ABSENCE_SUB_TYPE', data.absenceSubType)
+      if (!data.absenceSubType.nextDomain) {
+        saveCategorySubJourney(req)
+        return 'single-or-repeating'
+      }
+      return getUrlForNextDomain(data.absenceSubType.nextDomain)
+    }
+    if (data.reasonCategory) {
+      updateCategorySubJourney(req, 'ABSENCE_REASON_CATEGORY', data.reasonCategory)
+      if (!data.reasonCategory.nextDomain) {
+        saveCategorySubJourney(req)
+        return 'single-or-repeating'
+      }
+      return getUrlForNextDomain(data.reasonCategory.nextDomain)
+    }
+    if (data.reason) {
+      updateCategorySubJourney(req, 'ABSENCE_REASON', data.reason)
+      saveCategorySubJourney(req)
+      return 'single-or-repeating'
+    }
+
     if (data.repeat !== undefined) {
       journey.repeat = data.repeat
       return data.repeat ? 'start-end-dates' : 'start-date'
