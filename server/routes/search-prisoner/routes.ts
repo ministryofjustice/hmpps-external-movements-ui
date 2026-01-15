a0bfa8b3030fb8339166f945f2c90dfdd7afba0f
bill-moj <167539377+bill-moj@users.noreply.github.com>
2026-01-15 15:56:19 +0000

    Update search prisoner page (#226)

--- server/routes/search-prisoner/routes.ts
+++ server/routes/search-prisoner/routes.ts
@@ -2,12 +2,14 @@ import { Services } from '../../services'
 import { BaseRouter } from '../common/routes'
 import { Page } from '../../services/auditService'
 import { SearchPrisonerController } from './controller'
+import { validateOnGET } from '../../middleware/validation/validationMiddleware'
+import { schema } from './schema'
 
 export const SearchPrisonerRoutes = ({ prisonerSearchService }: Services) => {
   const { router, get } = BaseRouter()
   const controller = new SearchPrisonerController(prisonerSearchService)
 
-  get('/', Page.SEARCH_PRISONER, controller.GET)
+  get('/', Page.SEARCH_PRISONER, validateOnGET(schema, 'searchTerm'), controller.GET)
 
   return router
 }
