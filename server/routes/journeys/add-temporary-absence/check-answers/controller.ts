945482699c41c07d55ebac8dd734a2dfac8f9a45
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-02-19 15:21:54 +0000

    fix

--- server/routes/journeys/add-temporary-absence/check-answers/controller.ts
+++ server/routes/journeys/add-temporary-absence/check-answers/controller.ts
@@ -79,6 +79,7 @@ export class AddTapCheckAnswersController {
           weeklyPattern: journeyData.patternType === 'WEEKLY' ? journeyData.weeklyPattern : undefined,
           biweeklyPattern: journeyData.patternType === 'BIWEEKLY' ? journeyData.biweeklyPattern : undefined,
           shiftPattern: journeyData.patternType === 'SHIFT' ? journeyData.shiftPattern : undefined,
+          absencesPerDay: journeyData.absencesPerDay ?? 1,
         }
       }
 
