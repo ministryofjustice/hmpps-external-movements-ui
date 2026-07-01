dbd8e73907d4616ea003cb0a09b6d4747990a18c
bill-moj <bill.yeung@digital.justice.gov.uk>
2026-07-01 17:31:04 +0100

    Create occurrences when extending TAP date range

--- server/routes/journeys/temporary-absence-authorisations/edit/autofill-occurrences/routes.ts
+++ server/routes/journeys/temporary-absence-authorisations/edit/autofill-occurrences/routes.ts
@@ -1,13 +1,15 @@
 import { BaseRouter } from '../../../../common/routes'
 import { EditTapAutofillOccurrencesController } from './controller'
 import { Services } from '../../../../../services'
+import { validate } from '../../../../../middleware/validation/validationMiddleware'
+import { schema } from './schema'
 
 export const EditTapAutofillOccurrencesRoutes = ({ externalMovementsService }: Services) => {
   const { router, get, post } = BaseRouter()
   const controller = new EditTapAutofillOccurrencesController(externalMovementsService)
 
   get('/', controller.GET)
-  post('/', controller.POST)
+  post('/', validate(schema), controller.POST)
 
   return router
 }
